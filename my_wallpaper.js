77207f13217943ebcdf71c6975f9a42057f1e197
floorworm <lorimelucy@myvuw.ac.nz>
2024-08-04 21:28:23 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,8 +1,16 @@
 //your parameter variables go here!
 
-let SQSI = 20 //Square Size
-let SR= 5//roundedness of squares
-let LSR=1 //roundness of lilac
+let SQSI = 20; //Square Size
+
+let SR= 5;//roundedness of squares
+let LSR=1; //roundness of lilac
+
+let SquareX = 140;//Xvalue of Lilac
+let SquareY =0;//Yvalue of Lilac
+
+let SquareMx = 0//Xvalue of Marigold
+let SquareMy = 40//Yvalue of Marigold
+
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -26,6 +34,10 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 DrawGridBase(fill(245, 247, 188))//Yellow)
 
 DrawLilacFlower()
+
+DrawMarigoldFlower()
+
+
 
 }
  
@@ -160,44 +172,91 @@ function DrawGridBase(){
 function DrawLilacFlower(){
   
   
-  strokeWeight(0.9);
+  strokeWeight(0.6);
 fill(157, 127, 245)//Lilac
 
-square(60,20,SQSI,LSR)
-square(120,20,SQSI,LSR)
-square(40,20,SQSI,LSR)
-square(140,20,SQSI,LSR)
-square(60,40,SQSI,LSR)
-square(120,40,SQSI,LSR)
-square(80,40,SQSI,LSR)
-square(100,40,SQSI,LSR)
-square(60,60,SQSI,LSR)
+square(SquareX-40,SquareY,SQSI,LSR)
+square(SquareX+20,SquareY,SQSI,LSR)
+square(SquareX-60,SquareY,SQSI,LSR)
+square(SquareX+40,SquareY,SQSI,LSR)
+square(SquareX-40,SquareY+20,SQSI,LSR)
+square(SquareX+20,SquareY+20,SQSI,LSR)
+square(SquareX-20,SquareY+20,SQSI,LSR)
+square(SquareX,SquareY+20,SQSI,LSR)
+square(SquareX-40,SquareY+40,SQSI,LSR)
 
 
 fill(91, 55, 158)//DarkPurp
 
-square(80,60,SQSI,LSR)
-square(100,60,SQSI,LSR)
+square(SquareX-20,SquareY+40,SQSI,LSR)
+square(SquareX,SquareY+40,SQSI,LSR)
 
 fill(143, 171, 87)//LGreen
 
-square(120,60,SQSI,LSR)
-square(100,80,SQSI,LSR)
-square(80,120,SQSI,LSR)
-square(100,160,SQSI,LSR)
+square(SquareX+20,SquareY+40,SQSI,LSR)
+square(SquareX,SquareY+60,SQSI,LSR)
+square(SquareX-20,SquareY+100,SQSI,LSR)
+square(SquareX,SquareY+140,SQSI,LSR)
 
 
 fill(53, 79, 32)//DGreen
 
-square(80,80,SQSI,LSR)
-square(100,100,SQSI,LSR)
-square(80,140,SQSI,LSR)
+square(SquareX-20,SquareY+60,SQSI,LSR)
+square(SquareX,SquareY+80,SQSI,LSR)
+square(SquareX-20,SquareY+120,SQSI,LSR)
 
 fill(247, 208, 79)//Yellow
 
-square(80,20,SQSI,LSR)
-square(100,20,SQSI,LSR)
+square(SquareX-20,SquareY,SQSI,LSR)
+square(SquareX,SquareY,SQSI,LSR)
 
 
 
+}
+
+function DrawMarigoldFlower(){
+
+
+  strokeWeight(0.6);
+  fill(240, 107, 55)//Orange
+  
+  square(SquareMx+60,SquareMy,SQSI,LSR)
+  square(SquareMx+40,SquareMy,SQSI,LSR)
+  square(SquareMx+20,SquareMy,SQSI,LSR)
+  square(SquareMx+80,SquareMy+20,SQSI,LSR)
+  square(SquareMx+40,SquareMy+40,SQSI,LSR)
+  square(SquareMx,SquareMy+20,SQSI,SR)
+  square(SquareMx+100,SquareMy+20,SQSI,LSR)
+  square(SquareMx+20,SquareMy+40,SQSI,LSR)
+  
+  
+  fill(186, 58, 7)//Red
+  
+  square(SquareMx+20,SquareMy+20,SQSI,LSR)
+  square(SquareMx+60,SquareMy+40,SQSI,LSR)
+  square(SquareMx+80,SquareMy,SQSI,LSR)
+  
+  
+  
+  fill(143, 171, 87)//LGreen
+  
+  square(SquareMx+80,SquareMy+40,SQSI,LSR)
+  square(SquareMx+40,SquareMy+60,SQSI,LSR)
+  square(SquareMx+60,SquareMy+100,SQSI,LSR)
+  square(SquareMx+40,SquareMy+140,SQSI,LSR)
+  
+  
+  
+  fill(53, 79, 32)//DGreen
+  
+  square(SquareMx+60,SquareMy+60,SQSI,LSR)
+  square(SquareMx+40,SquareMy+120,SQSI,LSR)
+  square(SquareMx+40,SquareMy+80,SQSI,LSR)
+  
+  fill(247, 208, 79)//Yellow
+  
+  square(SquareMx+60,SquareMy+20,SQSI,LSR)
+  square(SquareMx+40,SquareMy+20,SQSI,LSR)
+  
+  
 }
